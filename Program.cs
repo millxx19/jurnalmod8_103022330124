2dc4bc9a7b3038508a2c0bca1a6117205ac64990
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-17 10:24:15 +0700

    commiit awal

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World !");
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        BankTransferConfig config = BankTransferConfig.Load();
+        //
+        config.UbahSatuan();
+        config.Save();
+
+        Console.Write($"Please insert the amount of money to transfer:"){config.satuan}: } ");
+        double jumlah = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("“Masukkan jumlah uang yang akan di-transfer:");  
+        int jumlah = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(transferBerhasil, jumlah, config.pesan_diterima ; config.pesan_ditolak);
+}
